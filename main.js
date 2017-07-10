d90e4d000b2a45d38b837fed5d11a68332aa7bae
Tomek <tomasz.duda.krk@gmail.com>
2017-07-10 20:05:40 +0200

    added before js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-main.js
+var location = {
+		//methods
+		success: function(pos){
+			var crd = pos.coords;
+	 	 	console.log(crd.lat);
+		},
+		getLocation: function(){
+			navigator.geolocation.getCurrentPosition(location.success);
+		},
+	
+
+	}
+
+location.getLocation();
